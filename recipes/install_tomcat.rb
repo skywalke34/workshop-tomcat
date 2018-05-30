9e3e878f238171e09263157595ef3faf4240a511
skywalke34 <skywalke34@hotmail.com>
2018-05-29 22:25:15 -0600

    cleanup
    ran cookstyle and cleaned up issues.
    Created array for setting owner/grp & permissions for directories

--- recipes/install_tomcat.rb
+++ recipes/install_tomcat.rb
@@ -12,15 +12,13 @@
 # Set the download URL for tomcat version using attributes
 tomcat_version = node['tomcat']['version']
 major_version = tomcat_version[0]
-#download_url = "#{node['tomcat']['download_server']}/dist/tomcat/tomcat-8/v8.5.20/bin/apache-tomcat-8.5.20.tar.gz"
 tarball = "apache-tomcat-#{tomcat_version}.tar.gz"
 download_file = "#{node['tomcat']['download_server']}dist/tomcat/tomcat-#{major_version}/v#{tomcat_version}/bin/apache-tomcat-#{tomcat_version}.tar.gz"
 
 # Create group
-# group node['tomcat']['group']
 group 'add_tomcat_grp' do
-   group_name 'tomcat'
-   action :create
+  group_name 'tomcat'
+  action :create
 end
 
 # Create user
@@ -28,39 +26,37 @@ user 'tomcat' do
   comment 'tomcat user'
   group 'tomcat'
   system true
-  password 'tomcat34!'
   home '/opt/tomcat'
   shell '/bin/bash'
   action :create
 end
 
 directory '/opt/tomcat' do
-    owner 'tomcat'
-    group 'tomcat'
-    mode '0755'
-    action :create
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0755'
+  action :create
 end
 
-#execute 'install_java' do
-#    command 'sudo yum install java-1.7.0-openjdk-devel'
-
+# Install Java
 yum_package 'install_java' do
-    package_name 'java-1.8.0-openjdk-devel'
-    action :install
+  package_name 'java-1.8.0-openjdk-devel'
+  action :install
 end
 
+# Install wget
 yum_package 'install_wget' do
-    package_name 'wget'
-    action :install
+  package_name 'wget'
+  action :install
 end
 
+# Install unzip
 yum_package 'install unzip' do
-    package_name 'unzip'
-    action :install
+  package_name 'unzip'
+  action :install
 end
 
 # Download and unpack tomcat
-#remote_file "#{Chef::config[:file_cache_path]}/#{tarball}"
 remote_file "/tmp/#{tarball}" do
   source download_file
   action :create_if_missing
@@ -68,70 +64,44 @@ remote_file "/tmp/#{tarball}" do
 end
 
 execute 'tar-file' do
-    command 'sudo tar xvf /tmp/apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
-    action :run
+  command 'sudo tar xvf /tmp/apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1'
+  action :run
 end
 
-execute 'update-perm' do
-    command 'sudo chgrp -R tomcat /opt/tomcat'
+directory '/opt/tomcat' do
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0755'
+  recursive true
+  action :create
 end
 
-execute 'update-r-conf' do
-    cwd '/opt/tomcat'
-    command 'sudo chmod -R g+r conf'
-    action :run
+directory '/opt/tomcat/conf' do
+  owner 'tomcat'
+  group 'tomcat'
+  mode '0750'
+  recursive true
+  action :create
 end
 
-execute 'update-x-conf' do
-    cwd '/opt/tomcat'
-    command 'sudo chmod g+x conf'
-    action :run
-end
-
-execute 'chown-directories' do
-    cwd '/opt/tomcat'
-    command 'sudo chown -R tomcat webapps/ work/ temp/ logs/' 
-    action :run
+# Update each directory owner and group to tomcat and verify permissions
+%w(/opt/tomcat/webapps /opt/tomcat/work /opt/tomcat/temp /opt/tomcat/logs).each do |path|
+  directory path do
+    owner 'tomcat'
+    group 'tomcat'
+    mode '0750'
+  end
 end
 
 template '/etc/systemd/system/tomcat.service' do
-    source 'tomcat-systemd.erb'
-    mode '0440'
-    owner 'root'
-    group 'root'
+  source 'tomcat-systemd.erb'
+  mode '0440'
+  owner 'root'
+  group 'root'
 end
 
-# Tomcat init script configuration
-# template "/etc/init.d/tomcat#{major_version}" do
-# source 'init.conf.erb'
-# mode '0755'
-#  owner node['tomcat']['user']
-#  group node['tomcat']['group']
-# end
-
-# include_recipe 'tomcat::set_tomcat_home'
-
-# Create default catalina.pid file to prevent restart error for 1st run of coookbook.
-# file "#{node['tomcat']['tomcat_home']}/catalina.pid" do
-#  owner node['tomcat']['user']
-#  group node['tomcat']['group']
-#  mode '0755'
-#  action :create
-#  not_if { ::File.exist?("#{node['tomcat']['tomcat_home']}/catalina.pid") }
-# end
-
-# Enabling tomcat service and restart the service if subscribed template has changed.
- # subscribes :restart, "template[#{node['tomcat']['tomcat_home']}/bin/setenv.sh]", :delayed
- # subscribes :restart, "template[#{node['tomcat']['tomcat_home']}/conf/server.xml]", :delayed
- # subscribes :restart, "template[/etc/init.d/tomcat#{major_version}]", :delayed
-
-# systemd_unit 'tomcat.service' do
-#     user 'root'
-#     action :reload_or_restart
-# end
-
-service "tomcat" do
-  supports :restart => true
+service 'tomcat' do
+  supports restart: true
   action :enable
-  subscribes :restart, "template[/etc/systemd/system/tomcat.service]", :delayed
+  subscribes :restart, 'template[/etc/systemd/system/tomcat.service]', :delayed
 end
